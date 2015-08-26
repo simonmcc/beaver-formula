4c97c7b24062f4650026ed2a5a90853b11c2673e
Simon McCartney <simon.mccartney@hp.com>
2015-08-26 23:05:08 +0100

    Updated serverspec to comply with Serverspec v2 & RSpec 3
    Change-Id: I632e4cd62fa89f6bed3b3c927628e3618d9335c0

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source 'https://rubygems.org'
 
 gem "test-kitchen"
 gem "kitchen-ec2"
+gem "kitchen-lxc"
+gem "kitchen-vagrant"
 gem "kitchen-salt", :path => '../kitchen-salt'
--- test/integration/default/serverspec/beaver_spec.rb
+++ test/integration/default/serverspec/beaver_spec.rb
@@ -1,19 +1,9 @@
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.path = '/sbin:/usr/sbin'
-  end
-end
+# Set backend type
+set :backend, :exec
 
 describe "beaver log shipper" do
-
-  it "has a running service of beaver" do
-    expect(service("beaver")).to be_running
-  end
 
   describe service('beaver') do
       it { should be_enabled   }

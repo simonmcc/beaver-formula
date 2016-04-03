7e3e592dee6d5fabd447bae9f5ea1ecf61a8d9ee
Simon McCartney <simon@mccartney.ie>
2016-04-03 23:18:43 +0100

    Update serverspec for tcp-output suite
    Change-Id: I1d225e2be53a74e735f6a31da1cfff4500060f14

--- test/integration/tcp-output/serverspec/beaver_spec.rb
+++ test/integration/tcp-output/serverspec/beaver_spec.rb
@@ -1,13 +1,7 @@
 require 'serverspec'
 
-include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.path = '/sbin:/usr/sbin'
-  end
-end
+# Set backend type
+set :backend, :exec
 
 describe "beaver log shipper" do
 
